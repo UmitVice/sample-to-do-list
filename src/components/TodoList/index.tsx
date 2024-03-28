f24f0a9d68b603125fdc7170d42b508e273138c6
Umit Vice <umitvice@gmail.com>
2024-03-28 07:41:05 +0300

    Change todoList.scss file (just .todo-container left)

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoForm from 'components/TodoForm';
 import TodoItem from 'components/TodoItem';
+import './todoList.scss';
 
 interface Todo {
   id: number;
@@ -18,7 +19,7 @@ const TodoList: React.FC = () => {
   const fetchTodos = async () => {
     try {
       const response = await axios?.get<Todo[]>('http://localhost:4000/api/tasks');
-      setTodos(response.data);
+      setTodos(response?.data);
     } catch (error) {
       console.error('Error fetching todos: ', error);
     }
@@ -27,7 +28,7 @@ const TodoList: React.FC = () => {
   const handleAddTodo = async (task: string) => {
     try {
       const response = await axios?.post<Todo>('http://localhost:4000/api/tasks', { task });
-      setTodos([...todos, response.data]);
+      setTodos([...todos, response?.data]);
     } catch (error) {
       console.error('Error adding todo: ', error);
     }
@@ -36,17 +37,19 @@ const TodoList: React.FC = () => {
   const handleDeleteTodo = async (id: number) => {
     try {
       await axios?.delete(`http://localhost:4000/api/tasks/${id}`);
-      setTodos(todos?.filter(todo => todo.id !== id));
+      setTodos(todos?.filter(todo => todo?.id !== id));
     } catch (error) {
       console.error('Error deleting todo: ', error);
     }
   };
 
   return (
-    <div>
-      <h1>ToDo List</h1>
-      <TodoForm onAddTodo={handleAddTodo} />
-      <ul>
+    <div className="todo-container">
+      <h1 className="todo-header">ToDo List</h1>
+      <div className="todo-form-container">
+        <TodoForm onAddTodo={handleAddTodo} />
+      </div>
+      <ul className="todo-list">
         {todos?.map(todo => (
           <TodoItem key={todo?.id} todo={todo} onDeleteTodo={handleDeleteTodo} />
         ))}
